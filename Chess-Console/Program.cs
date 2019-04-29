d6e8332c62153addb9b71e3910674bae5e20802b
Jefferson Morais <jeffmorais96@gmail.com>
2019-04-28 23:49:27 -0300

    Add project files.

---
+++ Chess-Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chess_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
