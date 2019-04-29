756eb4ea93ffe06021390f5bc118c4594cace721
Jefferson Morais <jeffmorais96@gmail.com>
2019-04-29 01:00:53 -0300

    Criando classe ChessPosition

---
+++ Chess-Console/Chess/ChessPosition.cs
@@ -0,0 +1,26 @@
+﻿using Tabuleiro;
+
+namespace Chess
+{
+    class ChessPosition
+    {
+        public char Column { get; set; }
+        public int Row { get; set; }
+
+        public ChessPosition(char column, int row)
+        {
+            Column = column;
+            Row = row;
+        }
+
+        public Position toPosition()
+        {
+            return new Position(8 - Row, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Row;
+        }
+    }
+}
